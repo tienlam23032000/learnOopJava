584392800f266fa98b162a1adf925bddd8f1f315
tienlam23032000 <tienlam23032000@gmail.com>
2023-10-09 21:21:58 +0700

    Initial commit

---
+++ src/main/java/com/likelion/learnoopjava/Person.java
@@ -0,0 +1,2 @@
+package com.likelion.learnoopjava;public class Person {
+}
---
+++ src/test/java/com/likelion/learnoopjava/LearnOopJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.learnoopjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOopJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
